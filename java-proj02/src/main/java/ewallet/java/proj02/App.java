9ea91cec2b400df2c24b8ea45cba0550eb88cd0a
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2022-04-10 13:10:03 -0400

    started with barebones javafx with notes

--- java-proj02/src/main/java/ewallet/java/proj02/App.java
+++ java-proj02/src/main/java/ewallet/java/proj02/App.java
@@ -4,7 +4,11 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -18,9 +22,22 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        StackPane sp = new StackPane(label);
-        var scene = new Scene(sp, 640, 480);
+//        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        GridPane gpWalletView = new GridPane();
+        VBox vbNotesPanel = new VBox();
+        Text tMonthInputLabel = new Text("Month");
+        Text tDayInputLabel = new Text("Day");
+        Text tYearInputLabel = new Text("Year");
+        TextField tfMonthInput = new TextField();
+        TextField tfDayInput = new TextField();
+        TextField tfYearInput = new TextField();
+        ComboBox cbNoteDropdown = new ComboBox();
+        Text noteDescription = new Text();
+        vbNotesPanel.getChildren().addAll(new Text("My Notes"), cbNoteDropdown, tMonthInputLabel, tfMonthInput, tDayInputLabel, tfDayInput, tYearInputLabel, tfYearInput);
+        gpWalletView.add(vbNotesPanel, 0, 1);
+
+
+        var scene = new Scene(gpWalletView, 640, 480);
 
 
         stage.setScene(scene);
