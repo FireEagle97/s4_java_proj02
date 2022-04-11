99282a594ddd77a57ded12f8917cc9dad2407fcb
dany <dany.makhoul@dawsoncollege.qc.ca>
2022-04-11 18:56:58 -0400

    observer pattern implemetation succeed

--- java-proj02/src/main/java/TestAkshan.java
+++ java-proj02/src/main/java/TestAkshan.java
@@ -5,52 +5,24 @@ import java.util.List;
 public class TestAkshan {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         //Card card1 = new PaymentCard("001", 2000, 2022,04, "Dany Makhoul", "123456");
         //Card card2 = new PersonalCard("Kyle veloso", "9881234", 2025,03, "Health card");
         CreditCard card3 = new CreditCard("001", 2000,04,2022, "Dany Makhoul", "123456");
         System.out.println(card3);
-        //Note note1 = new Note("2022")
-        //Note note2
-        //System.out.println(card1);
-        //System.out.println(card2);
-//        CreditCardView view = new CreditCardView();
-//        
-//        LimitNotifier notifier = new LimitNotifier();
-//        notifier.attach(view);
-//        notifier.notifyUpdate(card3);
+        CreditCardView view = new CreditCardView();
         
-        
-        
-        
-=======
-        Card card1 = new DebitCard("001", 2000, 2022,04, "Dany Makhoul", "123456");
-        Card card3 = new CreditCard("002", 3000,2022,07,"Kyle veloso","245456");
-        Card card2 = new PersonalCard("Kyle veloso", "9881234", 2025,03, "Healthcare card");
-        Note note1 = new Note(29,04,2022,"you have a dentist appointment");
-        Note note2 = new Note(15,04,2022,"don't forget to go to Costco");
-        System.out.println(card3);
-        System.out.println(card3);
-//        List<Card> cardsList = new ArrayList<>();
-//        List<Note> notesList = new ArrayList<>();
-//        EWallet wal1 = new EWallet(cardsList, notesList, 200);
-//        System.out.println("cash"+wal1.getCash());
-//        wal1.addNote(note1);
-//        wal1.addNote(note2);
-//        wal1.listNotes();
-//        wal1.deleteNote(note1);
-//        System.out.println("after deletion");
-//        wal1.listNotes();
-//        wal1.addCard(card1);
-//        wal1.addCard(card2);
-//         List<Card> cardsafter = wal1.getCardList();
-//        for (Card aCard: cardsafter){
-//            System.out.println(aCard);
-//            
-//        }
-                
->>>>>>> 49aef413e46ac8feaff3fffb59e84830d6520a3d
-        
+        LimitNotifier notifier = new LimitNotifier();
+        notifier.attach(view);
+        //card3.setAmountOwed(1500);
+        notifier.notifyUpdate(card3);
+//        Card card1 = new DebitCard("001", 2000, 2022,04, "Dany Makhoul", "123456");
+//        //Card card3 = new CreditCard("002", 3000,2022,07,"Kyle veloso","245456");
+//        Card card2 = new PersonalCard("Kyle veloso", "9881234", 2025,03, "Healthcare card");
+//        Note note1 = new Note(29,04,2022,"you have a dentist appointment");
+//        Note note2 = new Note(15,04,2022,"don't forget to go to Costco");
+//        System.out.println(card3);
+//        System.out.println(card3);
+
         
                    
     }
